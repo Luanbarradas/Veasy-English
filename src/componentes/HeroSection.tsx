51a36e13dae59f2a47ce7580c94c96e997a758fb
marcelo-dds <marcelodds_web@hotmail.com>
2024-07-16 18:54:23 -0300

    feat(heroSection):changed text color

--- src/componentes/HeroSection.tsx
+++ src/componentes/HeroSection.tsx
@@ -4,15 +4,15 @@ export const HeroSection = () => {
     return (
         <section className="h-[37.5rem] bg-cover bg-center flex flex-row justify-between items-center px-12 mt-16">
             <div className="text-left">
-                <h1 className="text-4xl md:text-6xl lg:text-8xl font-bold text-black">
+                <h1 className="text-4xl md:text-6xl lg:text-8xl font-bold text-purple-950">
                     Bem-vindo ao Veasy English
                 </h1>
                 <p className="text-lg md:text-xl lg:text-2xl text-black mt-4">
                     Aprenda inglês de forma fácil e divertida
                 </p>
-                <button className="mt-8 px-8 py-4 bg-purple-600 text-white text-lg font-semibold rounded hover:bg-purple-900 transition duration-300">
+                <a href="#"> <button className="mt-8 px-8 py-4 bg-purple-600 text-white text-lg font-semibold rounded hover:bg-purple-900 transition duration-300">
                     Comece Agora
-                </button>
+                </button></a>
             </div>
             <div className="h-full w-1/2 flex justify-center">
                 <img src={hero} alt="Hero" className="h-full object-contain" />
