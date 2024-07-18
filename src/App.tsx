39c77a960f7dcfe3098084fad2eb47dc7d82d73c
marcelo-dds <marcelodds_web@hotmail.com>
2024-07-17 22:34:37 -0300

    resolved conflict with APP

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { Header } from "./componentes/Header";
 import { HeroSection } from "./componentes/HeroSection.tsx";
 import { Plano } from "./componentes/Planos.tsx";
 import { Footer } from "./componentes/Footer.tsx";
-
-
 import { Main } from './componentes/Main.tsx';
 import './App.tsx'
 
