44e330178bf82cd456de6cf3e71831f1af5c74a4
marcelo-dds <marcelodds_web@hotmail.com>
2024-07-19 09:24:55 -0300

    feat:heroSection and heroSetion.css changed to raw css

--- src/componentes/HeroSection.tsx
+++ src/componentes/HeroSection.tsx
@@ -1,24 +1,25 @@
 import hero from "../assets/hero.svg";
+import '../styles/hero-section.css';
+
 
 export const HeroSection = () => {
   return (
-    <section className="h-[37.5rem] bg-cover bg-center flex flex-row justify-between items-center px-12 mt-16 bg-purple-100">
-      <div className="text-left">
-        <h1 className="text-4xl md:text-6xl lg:text-8xl font-bold text-purple-950">
+    <section className="hero-section">
+      <div className="hero-text">
+        <h1 className="hero-title">
           Bem-vindo ao Veasy English
         </h1>
-        <p className="text-lg md:text-xl lg:text-2xl text-black mt-4">
+        <p className="hero-subtitle">
           Aprenda inglês de forma fácil e divertida
         </p>
         <a href="#">
-          {" "}
-          <button className="mt-8 px-8 py-4 bg-orange-500 text-white text-lg font-semibold rounded hover:bg-orange-600 transition duration-300">
+          <button className="hero-button">
             Comece Agora
           </button>
         </a>
       </div>
-      <div className="h-full w-1/2 flex justify-center">
-        <img src={hero} alt="Hero" className="h-full object-contain" />
+      <div className="hero-image-container">
+        <img src={hero} alt="Hero" className="hero-image" />
       </div>
     </section>
   );
