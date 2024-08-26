6ceef7f2b511adb9b4949f3f6dd5b8c1a4b40a3e
marcelo-dds <marcelodds_web@hotmail.com>
2024-08-26 19:55:10 -0300

    feat(Planos):created modal added opacity to all css

--- src/componentes/Planos.tsx
+++ src/componentes/Planos.tsx
@@ -1,57 +1,113 @@
+import { useState } from "react";
 import planoIndividual from "../assets/aulaIndidual.jpg";
 import planoGrupo from "../assets/aulaGrupo.jpg";
 import conversacao from "../assets/conversacao.jpg";
+import theTalk from "../assets/thetalk.png";
+import aulaindividual from '../assets/aulaindividualumasemana.png';
+import aulaIndividualDuasSemanas from '../assets/aulaindividualdois.png';
+import aulaInvidualTresSemanas from '../assets/aulaindidualtres.png';
+import talk from '../assets/talkthetalk.png';
+
 import "../styles/plano.css";
 
 const planos = [
   {
     title: "Plano Individual",
-    price: "R$189/mês",
+    price: "R$189,00/mês",
     image: planoIndividual,
+    modalImage: aulaindividual
   },
   {
-    title: "Plano em Grupo",
-    price: "R$200/mês",
+    title: "Plano individual 2x",
+    price: "R$299,90/mês",
     image: planoGrupo,
+    modalImage: aulaIndividualDuasSemanas
   },
   {
-    title: "Conversação",
-    price: "R$150/mês",
+    title: "Plano indivual 3x",
+    price: "R$380,00/mês",
     image: conversacao,
-
+    modalImage: aulaInvidualTresSemanas
   },
   {
     title: "talk the talk",
-    price: 'R$150/mês',
-    //image:
+    price: 'R$159,90/mês',
+    image: theTalk,
+    modalImage: talk
   }
 ];
 
 export const Plano = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [currentImage, setCurrentImage] = useState<string | null>(null);
+
+
+  const openModal = (image: string) => {
+    setCurrentImage(image);
+    setIsOpen(true);
+    document.querySelector('.plano-section')?.classList.add('opacidade')
+    document.querySelector('.header')?.classList.add('opacidade')
+    document.querySelector('.hero-section')?.classList.add('opacidade')
+    document.querySelector('.main-section')?.classList.add('opacidade')
+
+
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+    setCurrentImage(null);
+    document.querySelector('.plano-section')?.classList.remove('opacidade')
+    document.querySelector('.header')?.classList.remove('opacidade')
+    document.querySelector('.hero-section')?.classList.remove('opacidade')
+    document.querySelector('.main-section')?.classList.remove('opacidade')
+
+
+
+  };
+
   return (
-    <section className="plano-section">
-      <h2 className="plano-title">Selecione um Plano</h2>
-      <div className="planos-container">
-        {planos.map((plano, index) => (
-          <div key={index} className="plano-card">
-            <div className="plano-image-wrapper">
-              <img
-                src={plano.image}
-                alt={plano.title}
-                className="plano-image"
-              />
+    <>
+      <section className="plano-section">
+        <h2 className="plano-title">Selecione um Plano</h2>
+        <div className="planos-container">
+          {planos.map((plano, index) => (
+            <div key={index} className="plano-card">
+              <div className="plano-image-wrapper">
+                <img
+                  src={plano.image}
+                  alt={plano.title}
+                  className="plano-image"
+                />
+              </div>
+              <div className="plano-card-botton">
+                <h3 className="plano-title-card">{plano.title}</h3>
+                <p className="plano-price">{plano.price}</p>
+                <label className="plano-button-label">
+                  <button
+                    className="button-pattern"
+                    onClick={() => openModal(plano.modalImage)}
+                  >
+                    Saiba mais
+                  </button>
+                </label>
+              </div>
             </div>
-            <div className="plano-card-botton">
-              <h3 className="plano-title-card">{plano.title}</h3>
-              <p className="plano-price">{plano.price}</p>
-              <label className="plano-button-label">
-                <input type="radio" name="plano" className="hidden" />
-                <span className="button-pattern">Saiba mais</span>
-              </label>
-            </div>
+          ))}
+        </div>
+      </section>
+      {/* Modal */}
+      {isOpen && (
+        <div className="modal-overlay" onClick={closeModal}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <button className="modal-close" onClick={closeModal}>
+              &times;
+            </button>
+            {currentImage && <img src={currentImage} alt="Plano Detalhe" />}
           </div>
-        ))}
-      </div>
-    </section>
+        </div>
+      )}
+
+    </>
+
   );
 };
